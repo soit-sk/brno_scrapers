b2ecbe03810eee59889de40b999d65496981b8a5
Michal Špaček <tupinek@gmail.com>
2015-10-11 14:40:18 +0200

    Fix data column.

--- scraper.pl
+++ scraper.pl
@@ -65,7 +65,7 @@ https://classic.scraperwiki.com/scrapers/subsidized_organizations_of_brno_city/,
 https://classic.scraperwiki.com/scrapers/firebrno/,firebrno,0,0
 https://morph.io/soit-sk/brno_average_daily_water_consumption_in_liters,Average daily water consumption in Brno,1,1
 https://morph.io/soit-sk/brno_sever_uredni_deska,Brno-Sever - Úřední deska,1,1
-https://morph.io/soit-sk/brno_subsidized_organizations_of_brno_city,Příspěvkové organizace, jejichž zřizovatelem je statutární město Brno a obchodní společnosti s majoritní majetkovou účastí statutárního města Brna,1,1
+https://morph.io/soit-sk/brno_subsidized_organizations_of_brno_city,"Příspěvkové organizace, jejichž zřizovatelem je statutární město Brno a obchodní společnosti s majoritní majetkovou účastí statutárního města Brna",1,1
 https://morph.io/soit-sk/brno_sever_departments,Brno-Sever - Odbory,1,1
 https://morph.io/soit-sk/firebrno,Hasičský záchranný sbor Jihomoravského kraje - Seznam událostí,1,1
 https://morph.io/soit-sk/firebrno_shortcuts,Hasičský záchranný sbor Jihomoravského kraje - Používané zkratky,1,1
